367764885976b2c49fb1adceddd520e94a244461
Mathew Araya Castillo <rafaelaraya121809@gmail.com>
2026-02-28 22:29:28 -0600

    subida de ejercicio#3
    Subida de archivos .cpp y .h del ejercicio 3

---
+++ sesion02/Ejercicio 3/Ejercicio 3/Matriz.cpp
@@ -0,0 +1,75 @@
+#include "Matriz.h"
+#include <iostream>
+
+const int filas = 3;
+const int columnas = 3;
+
+
+int datos[filas][columnas];
+
+void leerMatriz()
+{
+	std::cout << "Ingrese los valores para llenar la matriz: " << std::endl;
+
+	for (int i = 0; i < filas; i++) {
+		for (int j = 0; j < columnas; j++) {
+
+			std::cin >> datos[i][j];
+
+		}
+	}
+
+}
+
+void imprimirMatriz()
+{
+	for (int i = 0; i < filas; i++) {
+		
+		for (int j = 0; j < columnas; j++) {
+			std::cout<< " " << datos[i][j];
+		}
+		std::cout << " " << std::endl;
+	}
+
+
+}
+
+int sumarFilas()
+{
+
+	for (int i = 0; i < filas; i++) {
+		int suma = 0;
+		for (int j = 0; j < columnas; j++) {
+			suma = suma + datos[i][j];
+		}
+		std::cout << "El numero de fila " << i + 1 << " y su suma es " << suma << std::endl;
+	}
+
+
+	return 0;
+}
+
+int sumarColumnas()
+{
+
+	for (int j = 0; j < columnas; j++) {
+		int suma = 0;
+		for (int i = 0; i < filas ; i++) {
+			suma = suma + datos[i][j];
+		}
+		std::cout << "El numero de columna " << j + 1 << " y su suma es " << suma << std::endl;
+	}
+
+	return 0;
+}
+
+int sumarDiagonal()
+{
+	int suma = 0;
+	for (int i = 0; i < filas; i++) {
+		suma = suma + datos[i][i];
+	}
+	std::cout << "la suma de la diagonal es " << suma;
+
+	return 0;
+}
---
+++ sesion02/Ejercicio 3/Ejercicio 3/Matriz.h
@@ -0,0 +1,13 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+
+	void leerMatriz();
+	void imprimirMatriz();
+	int sumarFilas();
+	int sumarColumnas();
+	int sumarDiagonal();
+
+
+
+#endif
